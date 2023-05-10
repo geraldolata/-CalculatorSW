a361002aa90c2c7d3a7bc91d54aeb9065e894b7d
ArturDisha <133126792+geraldolata@users.noreply.github.com>
2023-05-10 15:42:07 +0200

    Initial Commit

---
+++ Calculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Calculator
+//
+//  Created by Artur Disha on 10.5.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
